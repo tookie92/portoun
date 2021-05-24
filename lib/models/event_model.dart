31d9b83160aaab611625ccd30c5f51dbf229434f
Joseph Ikinda <josephikinda@gmail.com>
2021-05-24 13:41:39 +0200

    ok

--- lib/models/event_model.dart
+++ lib/models/event_model.dart
@@ -3,21 +3,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class EventModel {
   String? id;
   String? title;
-  String? picture;
+
   String? author;
 
-  EventModel(this.title, this.author, this.picture);
+  EventModel(this.title, this.author);
 
   EventModel.fromSnapshot(DocumentSnapshot doc)
       : id = doc.id,
         title = doc.data()!['title'],
-        picture = doc.data()!['picture'],
         author = doc.data()!['author'];
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> map = {
       'title': this.title,
-      'picture': this.picture,
       'author': 'joseph ikinda',
     };
 
